7deef57a723a964f48ffb0c482e990dba22d87ed
mignon47 <qaztjudtjdud@naver.com>
2023-11-01 14:34:59 +0900

    [rename] : package rename

--- tickMate/src/main/java/com/test/ticketmate/TickMateApplication.java
+++ tickMate/src/main/java/com/test/ticketmate/TickMateApplication.java
@@ -1,4 +1,4 @@
-package com.test.wantedpreonboardingbackend;
+package com.test.ticketmate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- tickMate/src/test/java/com/test/ticketmate/TickMateApplicationTests.java
+++ tickMate/src/test/java/com/test/ticketmate/TickMateApplicationTests.java
@@ -1,4 +1,4 @@
-package com.test.wantedpreonboardingbackend;
+package com.test.ticketmate;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
